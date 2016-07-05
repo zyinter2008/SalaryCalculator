17bc09fc605b67560db6df281c2ab93a6a850a8c
zyin <zyin@zyindeMacBook-Pro.local>
2016-07-05 23:34:42 +0800

    unit test with param

--- test/salarycalculatortest.cpp
+++ test/salarycalculatortest.cpp
@@ -1,53 +1,42 @@
 #include "gtest/gtest.h"
 #include "salarycalculator.h"
 
-TEST(SalaryCalculatorTest, should_return_24000_when_xiren_for_baoyu_get_salary_of_jan) {
+class CalculatorParam {
+
+public:
+	string master;
+	string worker;
+	unsigned int month;
+	unsigned int salary;
+
+	CalculatorParam(const string &master, const string &worker,
+			unsigned int month, unsigned int salary) :
+			master(master), worker(worker), month(month), salary(salary) {
+	}
+
+};
+
+class SalaryCalculatorTest: public testing::TestWithParam<CalculatorParam> {
+
+};
+
+TEST_P(SalaryCalculatorTest, test_get_salary) {
+	CalculatorParam testPara = GetParam();
 	SalaryCalculator calc;
-	ASSERT_EQ(24000, calc.getSalary(WORKER_XI_REN,MASTER_BAO_YU, 1));
+	ASSERT_EQ(testPara.salary,
+			calc.getSalary(testPara.worker, testPara.master, testPara.month));
 }
 
-TEST(SalaryCalculatorTest, should_return_28000_when_xiren_for_baoyu_get_salary_of_april) {
-	SalaryCalculator calc;
-	ASSERT_EQ(28000, calc.getSalary(WORKER_XI_REN,MASTER_BAO_YU,4));
-}
-
-TEST(SalaryCalculatorTest, should_return_11200_when_qingwen_for_baoyu_get_salary_of_jan) {
-	SalaryCalculator calc;
-	ASSERT_EQ(11200, calc.getSalary(WORKER_QING_WEN,MASTER_BAO_YU, 1));
-}
-
-TEST(SalaryCalculatorTest, should_return_10400_when_qingwen_for_baoyu_get_salary_of_feb) {
-	SalaryCalculator calc;
-	ASSERT_EQ(10400, calc.getSalary(WORKER_QING_WEN,MASTER_BAO_YU, 2));
-}
-
-TEST(SalaryCalculatorTest, should_return_8000_when_sheyue_for_baoyu_get_salary_of_jan) {
-	SalaryCalculator calc;
-	ASSERT_EQ(8000, calc.getSalary(WORKER_SHE_YUE,MASTER_BAO_YU, 1));
-}
-
-TEST(SalaryCalculatorTest, should_return_10400_when_sheyue_for_baoyu_get_salary_of_july) {
-	SalaryCalculator calc;
-	ASSERT_EQ(10400, calc.getSalary(WORKER_SHE_YUE,MASTER_BAO_YU, 7));
-}
-
-TEST(SalaryCalculatorTest, should_return_20000_when_xiren_for_jiamu_get_salary_of_jan) {
-	SalaryCalculator calc;
-	ASSERT_EQ(20000, calc.getSalary(WORKER_XI_REN,MASTER_JIA_MU, 1));
-}
-
-
-TEST(SalaryCalculatorTest, should_return_8000_when_xiren_for_jiazheng_get_salary_of_jan) {
-	SalaryCalculator calc;
-	ASSERT_EQ(8000, calc.getSalary(WORKER_XI_REN,MASTER_JIA_ZHENG, 1));
-}
-
-TEST(SalaryCalculatorTest, should_return_4000_when_xueyan_for_daiyu_get_salary_of_jan) {
-	SalaryCalculator calc;
-	ASSERT_EQ(4000, calc.getSalary(WORKER_XUE_YAN,MASTER_DAI_YU, 1));
-}
-
-TEST(SalaryCalculatorTest, should_return_4000_when_wangmomo_for_daiyu_get_salary_of_jan) {
-	SalaryCalculator calc;
-	ASSERT_EQ(5500, calc.getSalary(WORKER_WANG_MO_MO,MASTER_DAI_YU, 1));
-}
+INSTANTIATE_TEST_CASE_P(SalaryCalculatorTest, SalaryCalculatorTest,
+		testing::Values(
+				CalculatorParam(MASTER_BAO_YU, WORKER_XI_REN, 1, 24000),
+				CalculatorParam(MASTER_BAO_YU, WORKER_XI_REN, 4, 28000),
+				CalculatorParam(MASTER_BAO_YU, WORKER_QING_WEN, 1, 11200),
+				CalculatorParam(MASTER_BAO_YU, WORKER_QING_WEN, 2, 10400),
+				CalculatorParam(MASTER_BAO_YU, WORKER_SHE_YUE, 1, 8000),
+				CalculatorParam(MASTER_BAO_YU, WORKER_SHE_YUE, 7, 10400),
+				CalculatorParam(MASTER_JIA_MU, WORKER_XI_REN, 1, 20000),
+				CalculatorParam(MASTER_JIA_ZHENG, WORKER_XI_REN, 1, 8000),
+				CalculatorParam(MASTER_DAI_YU, WORKER_XUE_YAN, 1, 4000),
+				CalculatorParam(MASTER_DAI_YU, WORKER_WANG_MO_MO, 1, 5500)
+				));
